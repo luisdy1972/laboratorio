470f6ea5bc706e2ac236eab8b69ec7d3faacdbb2
luisdy1972 <luisdy1972@hotmail.com>
2022-12-19 21:34:53 -0500

    practica pendiente

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const { json } = require("express");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+data = [
+  ({
+    id: 1,
+    coment: "bla, bla, bla",
+    important: true,
+  },
+  {
+    id: 2,
+    coment: "bla, bla, bla",
+    important: true,
+  },
+  {
+    id: 3,
+    coment: "bla, bla, bla",
+    important: true,
+  })
+];
+
+app.get("/", (req, res) => {
+  res.send();
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
 
 app.listen(port, () => {
-  console.log(`app runig en http://localhost:${port}`)
-})
+  console.log(`app runig en http://localhost:${port}`);
+});
